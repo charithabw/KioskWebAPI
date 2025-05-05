ff24fa8b69f96cf3702daa9b877e903578dc90c0
[charitha] <[charitha@agrithmics.lk]>
2025-05-05 16:39:54 +0530

    Add SP

---
+++ KioskWebAPI.DB/dbo/Stored Procedures/GetScreen.sql
@@ -0,0 +1,14 @@
+﻿
+CREATE PROCEDURE [dbo].[GetScreen]
+	
+AS
+BEGIN
+	
+	SET NOCOUNT ON;
+	SELECT [ScreenID]
+		  ,[ScreenCode]
+		  ,[ScreenName]		 
+	  FROM [dbo].[Screen]
+	WHERE IsActive = 1
+
+END
