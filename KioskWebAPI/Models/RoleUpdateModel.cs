593121c2a59e817eec310a560c4b5dcc204d84f7
Aroshana Dissanayake <aroshana6@gmail.com>
2025-07-06 18:51:54 +0530

    Implemented the RoleSaveModel and the RoleUpdateModel

---
+++ KioskWebAPI/Models/RoleUpdateModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Kiosk.WebAPI.Models
+{
+    public class RoleUpdateModel
+    {
+        public string? RoleName { get; set; }
+        public bool? IsActive { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public int? ModifiedBy { get; set; }
+    }
+}
