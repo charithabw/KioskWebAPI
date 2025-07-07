d529ccc91e468294a95119f4c775f0becdc2bf26
Aroshana Dissanayake <aroshana6@gmail.com>
2025-07-07 09:05:23 +0530

    Fix and update  Role Models Issues

--- KioskWebAPI/Models/RoleGetModel.cs
+++ KioskWebAPI/Models/RoleGetModel.cs
@@ -9,5 +9,9 @@
         public DateTime? CreatedDate { get; set; }
         public int? ModifiedBy { get; set; }
 
+        public DateTime? ModifiedDate { get; set; }
+        public int? CreatedBy { get; set; }
+
+
     }
 }
--- KioskWebAPI/Models/RoleUpdateModel.cs
+++ KioskWebAPI/Models/RoleUpdateModel.cs
@@ -4,7 +4,7 @@
     {
         public string? RoleName { get; set; }
         public bool? IsActive { get; set; }
-        public DateTime? CreatedDate { get; set; }
+        //public DateTime? CreatedDate { get; set; }
         public int? ModifiedBy { get; set; }
     }
 }
